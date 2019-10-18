a59649b61444174debd89df780adbf4a1033c1bf
Dorijan Tomić <dorijan11@gmail.com>
2019-10-18 19:44:04 +0200

    added background image

---
+++ src/containers/Layout/Layout.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import "./Layout.scss"
+export default class Layout extends Component {
+    render() {
+        return (
+            <div className="layout-container">
+                
+            </div>
+        )
+    }
+}
