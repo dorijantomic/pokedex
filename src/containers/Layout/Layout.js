b3ec52ac63c7e1ae3421eed161eecfe572b42079
Dorijan Tomić <dorijan11@gmail.com>
2019-10-18 20:17:35 +0200

    layout background complete

--- src/containers/Layout/Layout.js
+++ src/containers/Layout/Layout.js
@@ -4,7 +4,7 @@ export default class Layout extends Component {
     render() {
         return (
             <div className="layout-container">
-                
+                <audio src="https://instrumentalfx.co/wp-content/upload/11/Pokemon-Theme-Song.mp3?_=3" autoPlay={true} hidden={true} loop={true}/>
             </div>
         )
     }
